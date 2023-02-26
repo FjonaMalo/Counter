96c247ab5c507ae61d1c1b8677d50746fdbdeed1
fjonamalo <fjonamalo0@gmail.com>
2023-02-26 21:09:36 +0100

    Updating Arrays Immutably

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -29,6 +29,14 @@ const reducer = (state = inititalState, action) => {
         ...state,
         results: state.results.concat({ id: new Date(), value: state.counter }),
       };
+    case "DELETE_RESULT":
+      const updatedArray = state.results.filter(
+        (result) => result.id !== action.resultElId
+      );
+      return {
+        ...state,
+        results: updatedArray,
+      };
   }
   return state;
 };
