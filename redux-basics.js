84c3fc06f3361d922bce14524143aec4a49495cc
fjonamalo <fjonamalo0@gmail.com>
2023-02-26 18:56:14 +0100

    Dispatching action

--- redux-basics.js
+++ redux-basics.js
@@ -7,9 +7,26 @@ const inititalState = {
 
 // Reducer
 const rootReducter = (state = inititalState, action) => {
+  if (action.type === "INC_COUNTER") {
+    return {
+      ...state,
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "ADD_COUNTER") {
+    return {
+      ...state,
+      counter: state.counter + action.value,
+    };
+  }
   return state;
 };
 
 // Store
 const store = createStore(rootReducter);
+console.log(store.getState());
+
+// Dispatching action
+store.dispatch({ type: "INC_COUNTER" });
+store.dispatch({ type: "ADD_COUNTER", value: 10 });
 console.log(store.getState());
